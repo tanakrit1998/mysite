e90f75807b29df498c67bbd883a067866acab2e9
tanakrit1998 <tanakrit.ja.59@ubu.ac.th>
2020-02-11 18:23:54 +0700

    add

--- wakerfarmer/admin.py
+++ wakerfarmer/admin.py
@@ -3,9 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
-admin.site.register(Queue)
-admin.site.register(Map)
-admin.site.register(Price)
 admin.site.register(Mill)
 admin.site.register(Farmer)
+admin.site.register(Carqueue)
 admin.site.register(Ownermill)
--- wakerfarmer/models.py
+++ wakerfarmer/models.py
@@ -1,37 +1,16 @@
 from django.db import models
 
-class Queue(models.Model):
-    qid    = models.AutoField(primary_key = True)
-    queue  = models.IntegerField(null     = True, blank = True)
-
-    def __str__(self):
-        return f'{self.qid} - คิวที่ {self.queue}'
-
-class Price(models.Model):
-    pid    = models.AutoField(primary_key = True)
-    price  = models.IntegerField(null     = True, blank = True)
-    sprice = models.IntegerField(null     = True, blank = True)
-
-    def __str__(self):
-        return f'{self.pid} - ราคาข้าวหอมมะลิ {self.price} -ราคาข้าวเหนียว {self.sprice}'
-
-class Map(models.Model):
-    lid     = models.AutoField(primary_key = True)
-    lat     = models.FloatField(null    = True, blank = True, unique=True)
-    lng     = models.FloatField(null    = True, blank = True, unique=True)
-
-    def __str__(self):
-        return f'{self.lid}  ละติจูด {self.lat}  ลองจิจูด {self.lng}'
-
 class Mill(models.Model):
     mid    = models.AutoField(primary_key = True)
-    queue  = models.ForeignKey(Queue,on_delete=models.CASCADE,default=1)
+    queue  = models.IntegerField(null    = True,blank   = True)
     name   = models.CharField(max_length  = 100)
-    price  = models.ForeignKey(Price,on_delete=models.CASCADE,default=1)
-    map    = models.ForeignKey(Map,on_delete=models.CASCADE,default=1)
+    price  = models.IntegerField(null     = True,blank   = True)
+    sprice = models.IntegerField(null     = True, blank  = True)
+    lat    = models.FloatField(null  = True, blank = True)
+    lng    = models.FloatField(null  = True, blank = True)
  
     def __str__(self):
-        return f'{self.mid} - โรงสี - {self.name} - คิวที่ - {self.queue}  {self.price} - ละติจูด ลองจิจูด - {self.map} '
+        return f'{self.mid} - โรงสี {self.name} คิวที่ {self.queue} ราคาข้าวจ้าว {self.price} ราคข้าวเหนียว  {self.sprice} ละติจูด {self.lat} ลองจิจูด {self.lng}'
 
 class Farmer(models.Model):
     fid        = models.AutoField(primary_key = True)
@@ -55,4 +34,40 @@ class Ownermill(models.Model):
         return f'{self.oid} ชื่อ {self.first_name} นามสกุล {self.last_name} username: {self.username} password: {self.password} '        
 
 
+class ScoreLevel(models.Model):
+    sid     = models.AutoField(primary_key = True)
+    text    = models.CharField(max_length   = 100)
+    value   = models.IntegerField()
+
+class Score(models.Model):
+    sid     = models.AutoField(primary_key = True)
+    level  = models.ForeignKey(ScoreLevel,on_delete=models.CASCADE)
+    farmer  = models.ForeignKey(Farmer,on_delete=models.CASCADE)
+    mill  = models.ForeignKey(Mill,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.sid} คะแนน {self.score}'
+
+class Status(models.Model):
+    sid     = models.AutoField(primary_key = True)
+    text    = models.CharField(max_length   = 100) # start, during, done
+
+class Carqueue(models.Model):
+    cid    = models.AutoField(primary_key       = True)
+    queue  = models.IntegerField(null           = True,blank = True)
+    farmer = models.ForeignKey(Farmer,on_delete = models.CASCADE)
+    mill   = models.ForeignKey(Mill,on_delete   = models.CASCADE)
+    status = models.ForeignKey(Status,on_delete   = models.CASCADE)
+
+    def __str__(self):
+        return f'{self.cid} คิวที่ {self.queue}'
+
+class Ownermill(models.Model):
+    oid         = models.AutoField(primary_key = True)
+    first_name   = models.CharField(max_length = 100)
+    last_name    = models.CharField(max_length = 100)
+    username     = models.CharField(max_length = 100)
+    password     = models.CharField(max_length = 100)
+    def __str__(self):
+        return f'{self.oid} ชื่อ {self.first_name} นามสกุล {self.last_name} username: {self.username} password: {self.password}'
 
