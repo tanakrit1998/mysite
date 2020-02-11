d9102ca7b21e4f26fb57b7418b21064c90002eb1
tanakrit1998 <tanakrit.ja.59@ubu.ac.th>
2020-02-11 17:31:00 +0700

    add web database

--- wakerfarmer/admin.py
+++ wakerfarmer/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
+admin.site.register(Queue)
+admin.site.register(Map)
+admin.site.register(Price)
 admin.site.register(Mill)
 admin.site.register(Farmer)
-admin.site.register(Carqueue)
 admin.site.register(Ownermill)
